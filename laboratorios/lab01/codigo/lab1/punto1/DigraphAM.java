b1ebe8dff7b71aaef8a1eb3b2a248fe75ae9dd67
jjsanchezc <68928367+jjsanchezc@users.noreply.github.com>
2021-02-21 17:49:31 -0500

    Update DigraphAM.java

--- laboratorios/lab01/codigo/lab1/punto1/DigraphAM.java
+++ laboratorios/lab01/codigo/lab1/punto1/DigraphAM.java
@@ -2,33 +2,29 @@ package lab1.punto1;
 
 import java.util.ArrayList;
 
-
-public class DigraphAM extends Graph{
+public class DigraphAM extends Graph {
     Pareja[][] matrix;
-    
 
     public DigraphAM(int size) {
         super(size);
         matrix = new Pareja[size][size];
     }
 
-
     public void addArc(int source, int destino, double Peso, String nombre) {
         Pareja n = new Pareja(nombre, Peso);
         matrix[source][destino] = n;
     }
 
-
     public ArrayList<Integer> getSiguientes(int vertex) {
         ArrayList<Integer> Siguientes = new ArrayList<>();
-        for(int i = 0; i < size; i++){
-            if(matrix[vertex][i].peso != 0){
+        for (int i = 0; i < size; i++) {
+            if (matrix[vertex][i].peso != 0) {
                 Siguientes.add(i);
             }
         }
-        if(Siguientes.size() == 0){
+        if (Siguientes.size() == 0) {
             return null;
-        }         
+        }
         return Siguientes;
     }
 
